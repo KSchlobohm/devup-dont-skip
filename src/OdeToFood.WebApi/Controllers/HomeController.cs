f715938f24598284da01fe97c73f07fe08e147d6
Ken Schlobohm <keschlob@microsoft.com>
2023-08-18 13:54:50 -0500

    add external session

--- src/OdeToFood.WebApi/Controllers/HomeController.cs
+++ src/OdeToFood.WebApi/Controllers/HomeController.cs
@@ -9,8 +9,10 @@ namespace OdeToFood.WebApi.Controllers
     public class HomeController : Controller
     {
         // GET: Home
+        static int requestCount = 1;
         public ActionResult Index()
         {
+            Session[Guid.NewGuid().ToString()] = $"Hello world{requestCount++}";
             return View();
         }
     }
