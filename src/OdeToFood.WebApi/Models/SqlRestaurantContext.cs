44c6c1ad64314d32517892bb8eb27dfa18e0e7c1
Ken Schlobohm <keschlob@microsoft.com>
2023-08-18 16:53:48 -0500

    manage token

--- src/OdeToFood.WebApi/Models/SqlRestaurantContext.cs
+++ src/OdeToFood.WebApi/Models/SqlRestaurantContext.cs
@@ -1,10 +1,13 @@
 using OdeToFood.Data.Models;
+using System;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
 
 public class SqlRestaurantContext : DbContext
 {
     public DbSet<Restaurant> Restaurants { get; set; }
+
+    private static readonly System.Web.Caching.Cache _cache = new System.Web.Caching.Cache();
 
     public SqlRestaurantContext() : base("name=DefaultConnection")
     {
@@ -14,8 +17,24 @@ public class SqlRestaurantContext : DbContext
 
         var credential = new Azure.Identity.DefaultAzureCredential();
 
+#if DEBUG
+        string accessToken;
+        if (_cache["db_token"] as string is null)
+        {
+            var token = credential.GetToken(new Azure.Core.TokenRequestContext(new[] { "https://database.windows.net/.default" }));
+            accessToken = token.Token;
+            // Add the nonce to the cache with a short expiration time
+            _cache.Insert("db_token", accessToken, null, DateTime.UtcNow.AddMinutes(5), System.Web.Caching.Cache.NoSlidingExpiration);
+        }
+        else
+        {
+            accessToken = _cache["db_token"] as string;
+        }
+        conn.AccessToken = accessToken;
+#else
         var token = credential.GetToken(new Azure.Core.TokenRequestContext(new[] { "https://database.windows.net/.default" }));
         conn.AccessToken = token.Token;
+#endif
 
         // if using Microsoft.Data.SqlClient (default with EFCore)
         // then you can skip the above code and accomplish the same thing with just a connection string change
